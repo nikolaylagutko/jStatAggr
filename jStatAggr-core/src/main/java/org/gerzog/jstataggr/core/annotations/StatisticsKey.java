6e20510200290d4cd1e0f62ed3c07626fdb66c55
Nikolay Lagutko <nikolay.lagutko@mail.com>
2014-05-27 15:50:53 +0300

    enable licensing

--- jStatAggr-core/src/main/java/org/gerzog/jstataggr/core/annotations/StatisticsKey.java
+++ jStatAggr-core/src/main/java/org/gerzog/jstataggr/core/annotations/StatisticsKey.java
@@ -1,5 +1,17 @@
 /**
- * 
+ * Copyright (C)2014 - Nikolay Lagutko <nikolay.lagutko@mail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.gerzog.jstataggr.core.annotations;
 
@@ -10,10 +22,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks a field to be a part of Statistics Key. 
- * 
- * This mean that values of non-key field will be grouped by values of key fields.
- * 
+ * Marks a field to be a part of Statistics Key.
+ *
+ * This mean that values of non-key field will be grouped by values of key
+ * fields.
+ *
  * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
  *
  */
